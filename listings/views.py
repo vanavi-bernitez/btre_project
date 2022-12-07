f1a8e331e3aa566159622e9ae6a203ed2bf86d0f
Viviana Benitez <viviana.bernitez@gmail.com>
2022-12-07 13:00:34 -0500

    fetching data from db and display in the front-end

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Listing # llamar el archivo .models.py 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-# Create your views here.
+
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all().order_by('id') #this fetch all of the listings
+    paginator = Paginator(listings, per_page=3)
+    page = request.GET.get('page') #page is an URL parameter https://realpython.com/django-pagination/
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings  #pass it as a dictionary 
+    }
+    return render(request, 'listings/listings.html', context) #the second value is a dic, where im gonna display the inf to the fron
 
 def listing(request):
     return render(request, 'listings/listing.html')
