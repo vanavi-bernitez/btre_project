0abec54b84b3afd05339a00285d0bda2d464c855
Viviana Benitez <viviana.bernitez@gmail.com>
2022-12-19 17:08:01 -0500

    Background fixed: building and showcase now show up

--- pages/views.py
+++ pages/views.py
@@ -15,7 +15,7 @@ def name(took a request):
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
     context = {
-        'listings': listings,
+        'listings': listings, 
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices
